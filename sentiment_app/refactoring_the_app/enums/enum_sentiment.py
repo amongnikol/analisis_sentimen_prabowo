f865f321e48c6749e4e6adf51865017b7d9fd196
amongnikol <kismoyoakmal@gmail.com>
2025-07-17 18:29:05 +0700

    fix refactoring the app

--- sentiment_app/refactoring_the_app/enums/enum_sentiment.py
+++ sentiment_app/refactoring_the_app/enums/enum_sentiment.py
@@ -4,3 +4,13 @@ class SentimentEnum(Enum):
     NEUTRAL = "netral"
     POSITIVE = "positif"
     NEGATIVE = "negatif"
+
+    # karna output dari model int maka dibuatkan mapping
+    @classmethod
+    def from_index(cls, index: int) -> "SentimentEnum":
+        mapping = {
+            0: cls.NEUTRAL,
+            1: cls.POSITIVE,
+            2: cls.NEGATIVE
+        }
+        return mapping[index]
